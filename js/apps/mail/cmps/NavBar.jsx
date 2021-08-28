c7ee142462d7ed649a842bfd28026c6f18941652
shany.pollack <shany777@gmail.com>
2021-08-28 22:33:29 +0300

    edit css img

--- js/apps/mail/cmps/NavBar.jsx
+++ js/apps/mail/cmps/NavBar.jsx
@@ -9,10 +9,10 @@ export function NavBar({ countUnRead }) {
             <Route exact component={MailCompose} path="/mail/compose/:mailId?" />
             <Link to={`/mail/compose`} className="compose-btn pointer">Compose +</Link>
             <nav className="folders-nav flex pointer">
-                <NavLink to="/mail/inbox"><div><img src="../../../img/inbox.svg" /> inbox</div></NavLink>
-                <NavLink to="/mail/sent"><div><img src="../../../img/send.svg" />sent</div></NavLink>
-                <NavLink to="/mail/starred"><div><img src="../../../img/is-starred.svg" />starred</div></NavLink>
-                <NavLink to="/mail/drafts"><div><img src="../../../img/drafts.svg" />Drafts</div></NavLink>
+                <NavLink to="/mail/inbox"><div><img src="./././img/inbox.svg" /> inbox</div></NavLink>
+                <NavLink to="/mail/sent"><div><img src="./././img/send.svg" />sent</div></NavLink>
+                <NavLink to="/mail/starred"><div><img src="./././img/is-starred.svg" />starred</div></NavLink>
+                <NavLink to="/mail/drafts"><div><img src="./././img/drafts.svg" />Drafts</div></NavLink>
             </nav>
             <p className="count-read">Unread mails: {countUnRead}</p>
             </nav>
